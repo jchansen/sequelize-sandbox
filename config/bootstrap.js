6ce0f7f6f43b2f1e4cb4412bc4105dc73b2da768
jchansen <jason_hansen@outlook.com>
2015-05-10 00:53:49 -0700

    add sequelize models

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,9 +9,42 @@
  * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('database', 'username', 'password', {
+  dialect: 'sqlite',
+  storage: '.tmp/db.sqlite'
+});
+
 module.exports.bootstrap = function(cb) {
 
-  // It's very important to trigger this callback method when you are finished
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  var Project = sequelize.define('Project', {
+    title:       Sequelize.STRING,
+    description: Sequelize.TEXT
+  });
+
+  var Task = sequelize.define('Task', {
+    title:       Sequelize.STRING,
+    description: Sequelize.TEXT,
+    deadline:    Sequelize.DATE
+  });
+
+  Project.hasMany(Task);
+
+  sequelize.sync({force: true}).then(function() {
+    // bootstrap data
+    var task = Task.build({title: 'very important task'});
+    task.save()
+      .then(function() {
+        //console.log("success!");
+        // It's very important to trigger this callback method when you are finished
+        // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+        cb();
+      })
+      .error(function(err) {
+        console.log(err);
+      });
+  });
+
+
 };
