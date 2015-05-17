1eb4d35f45b358ae707df2e7b0e8abd2ca535422
jchansen <jason_hansen@outlook.com>
2015-05-17 03:23:16 -0700

    add comments

--- test/factories.js
+++ test/factories.js
@@ -4,24 +4,25 @@ var adapter = require('./factory-girl-sequelize-adapter')();
 // Setup sequelize
 var sequelize = require('./sequelize');
 
+// Require all the models in api/models
 var requireDir = require('require-dir');
 var models = requireDir('../api/models');
 
+// Setup FactoryGirl to use Sequelize
+var factory = new FactoryGirl.Factory();
+factory.setAdapter(adapter);
+
+var Models = {};
+
+Object.keys(models).map(function(modelName){
+  var model = models[modelName];
+  Models[modelName] = sequelize.define(modelName, model.attributes)
+  global[modelName] = Models[modelName];
+});
+
+require('./factories/project')(factory, Models['Project']);
+require('./factories/task')(factory, Models['Task']);
 
 module.exports = function() {
-  var factory = new FactoryGirl.Factory();
-  factory.setAdapter(adapter);
-
-  var Models = {};
-
-  Object.keys(models).map(function(modelName){
-    var model = models[modelName];
-    Models[modelName] = sequelize.define(modelName, model.attributes)
-    global[modelName] = Models[modelName];
-  });
-
-  require('./factories/project')(factory, Models['Project']);
-  require('./factories/task')(factory, Models['Task']);
-
   return factory;
 };
