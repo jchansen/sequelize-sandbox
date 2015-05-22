afe56b48fa2898f2491ef9fbdd95dbfa710cc2a1
jchansen <jason_hansen@outlook.com>
2015-05-21 19:42:00 -0700

    remove old sails start/stop bootstrap logic

--- test/integration2/controllers/project/index.test.js
+++ test/integration2/controllers/project/index.test.js
@@ -3,31 +3,6 @@ var expect = require('chai').expect;
 var factory = require('../../../factories')();
 var sequelize = require('../../../sequelize');
 
-//before(function(done){
-//
-//  // Load the app (no need to "lift" to a port)
-//  sails.load({
-//    log: {
-//      level: 'warn'
-//    },
-//    hooks: {
-//      grunt: false
-//    }
-//  }, function whenAppIsReady(err){
-//    if (err) return done(err);
-//
-//    // At this point, the `sails` global is exposed, although we
-//    // could have disabled it above with our config overrides to
-//    // `sails.load()`. In fact, you can actually use this technique
-//    // to set any configuration setting you like.
-//    return done();
-//  });
-//});
-//
-//after(function afterTestsFinish (done) {
-//  sails.lower(done);
-//});
-
 function rebuildDatabase(){
   return sequelize.sync({force: true});
 }
